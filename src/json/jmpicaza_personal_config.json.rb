038e4f3cc09393184dce0c602a4248714bc5606d
J. Manuel <jmpicaza@users.noreply.github.com>
2022-03-09 23:18:48 +0100

    Create jmpicaza_personal_config.json.rb
    Use pressed j, k, l, ; and . as shift, control, alt, command and altGr

---
+++ src/json/jmpicaza_personal_config.json.rb
@@ -0,0 +1,69 @@
+{
+    "type": "basic",
+    "from": {
+        "key_code": "j",
+        "modifiers": {
+            "optional": ["caps_lock"]
+        }
+    },
+    "parameters": {
+        "basic.to_if_alone_timeout_milliseconds": 250,
+        "basic.to_if_held_down_threshold_milliseconds": 250
+    },
+    "to_if_alone": [
+        {
+            "key_code": "j"
+        }
+    ],
+    "to_if_held_down": [
+        {
+            "key_code": "right_shift"
+        }
+    ]
+}
+{
+    "type": "basic",
+    "from": {
+        "key_code": "k",
+        "modifiers": {
+            "optional": ["caps_lock"]
+        }
+    },
+    "parameters": {
+        "basic.to_if_alone_timeout_milliseconds": 250,
+        "basic.to_if_held_down_threshold_milliseconds": 250
+    },
+    "to_if_alone": [
+        {
+            "key_code": "k"
+        }
+    ],
+    "to_if_held_down": [
+        {
+            "key_code": "right_control"
+        }
+    ]
+}
+{
+    "type": "basic",
+    "from": {
+        "key_code": "l",
+        "modifiers": {
+            "optional": ["caps_lock"]
+        }
+    },
+    "parameters": {
+        "basic.to_if_alone_timeout_milliseconds": 250,
+        "basic.to_if_held_down_threshold_milliseconds": 250
+    },
+    "to_if_alone": [
+        {
+            "key_code": "l"
+        }
+    ],
+    "to_if_held_down": [
+        {
+            "key_code": "alt"
+        }
+    ]
+}
